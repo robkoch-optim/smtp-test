b655f81209c8052d98af5218fe7ce4527ee47a48
robkoch-optim <robert.koch@optim.pl>
2025-12-06 22:36:01 +0100

    Create smtp-test.js

---
+++ smtp-test.js
@@ -0,0 +1,46 @@
+import nodemailer from "nodemailer";
+
+console.log("=== SMTP TEST START ===");
+
+const {
+    SMTP_HOST,
+    SMTP_PORT,
+    SMTP_USER,
+    SMTP_PASS,
+    TEST_EMAIL
+} = process.env;
+
+async function main() {
+    try {
+        console.log("Connecting to SMTP...");
+
+        const transporter = nodemailer.createTransport({
+            host: SMTP_HOST,
+            port: Number(SMTP_PORT),
+            secure: Number(SMTP_PORT) === 465, 
+            auth: {
+                user: SMTP_USER,
+                pass: SMTP_PASS
+            },
+            tls: { rejectUnauthorized: false }
+        });
+
+        await transporter.verify();
+        console.log("SMTP Verified ✓");
+
+        const info = await transporter.sendMail({
+            from: SMTP_USER,
+            to: TEST_EMAIL,
+            subject: "Render SMTP test",
+            text: "OK – działa",
+        });
+
+        console.log("Email sent ✓");
+        console.log(info);
+        
+    } catch (err) {
+        console.error("ERROR:", err);
+    }
+}
+
+main();
